7fa97815cb11b3f78c073567ef75b55db2cf0d57
Robin Vaaler <vaaler@me.com>
2019-06-04 14:15:14 +0200

    patch setup.py to work with conda

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@ from distutils.core import setup
 from distutils.extension import Extension
 from distutils.command.build_py import build_py as _build_py
 
-from os import system
+from os import system, environ
 
 from distutils.core import setup, Extension
+
+conda_env_path = environ['CONDA_PREFIX']
+
+assert conda_env_path is not None and len(conda_env_path) > 0, 'This setup.py is patched to work with conda environements see (https://github.com/openexr/openexr/issues/207#issuecomment-478425804)' 
 
 version = "1.3.2"
 setup(name='OpenEXR',
@@ -17,8 +21,8 @@ setup(name='OpenEXR',
   ext_modules=[ 
     Extension('OpenEXR',
               ['OpenEXR.cpp'],
-              include_dirs=['/usr/include/OpenEXR', '/usr/local/include/OpenEXR', '/opt/local/include/OpenEXR'],
-              library_dirs=['/usr/local/lib', '/opt/local/lib'],
+              include_dirs=[conda_env_path + '/include/OpenEXR'],
+              library_dirs=[conda_env_path + '/lib'],
               libraries=['Iex', 'Half', 'Imath', 'IlmImf', 'z'],
               extra_compile_args=['-g', '-DVERSION="%s"' % version])
   ],
